8b9d60738011508dfbb101ab28cede94f96e6f2f
Apple <Apple@Apples-MacBook-Air.local>
2015-12-27 16:01:46 +0700

    Initial Commit

---
+++ SwingShip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwingShip
+//
+//  Created by Apple on 12/27/15.
+//  Copyright (c) 2015 AMOSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
